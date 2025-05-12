09ffaf3107420fd585e2f2731832882286b7488a
arevik.agopian <arevik.agopian@delonia.com>
2025-05-12 22:12:56 +0400

    (new attempt)Admin page with DAO implementation created still has bugs - in progress

--- src/main/java/am/aua/resourcehub/servlets/AdminLoginServlet.java
+++ src/main/java/am/aua/resourcehub/servlets/AdminLoginServlet.java
@@ -3,29 +3,26 @@ package am.aua.resourcehub.servlets;
 import am.aua.resourcehub.DAO.AdminDAO;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.swing.*;
 import java.io.IOException;
 
-    @WebServlet("/adminLogin")
-    public class AdminLoginServlet extends HttpServlet {
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-            String username = request.getParameter("username");
-            String password = request.getParameter("pass");
+public class AdminLoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
 
-            AdminDAO adminDAO = new AdminDAO();
-            if (adminDAO.validate(username, password)) {
-                HttpSession session = request.getSession();
-                session.setAttribute("adminUser", username);
-                response.sendRedirect("adminDashboard.jsp");
-            } else {
-                request.setAttribute("errorMessage", "Invalid credentials");
-                request.getRequestDispatcher("adminLogin.jsp").forward(request, response);
-            }
+        AdminDAO adminDAO = new AdminDAO();
+        if (adminDAO.authenticate(username, password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute("username", username);
+            resp.sendRedirect("adminDashboard.jsp");
+        } else {
+            req.setAttribute("errorMessage", "Invalid username or password");
+            req.getRequestDispatcher("adminLogin.jsp").forward(req, resp);
         }
     }
 
+}
