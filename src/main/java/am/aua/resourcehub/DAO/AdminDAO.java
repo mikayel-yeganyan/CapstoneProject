dee00f97eeb95de5c5ac7cac6a44131065caccc4
arevik.agopian <arevik.agopian@delonia.com>
2025-05-12 22:12:56 +0400

    (new attempt)Admin page with DAO implementation created still has bugs - in progress

--- src/main/java/am/aua/resourcehub/DAO/AdminDAO.java
+++ src/main/java/am/aua/resourcehub/DAO/AdminDAO.java
@@ -1,25 +1,26 @@
 package am.aua.resourcehub.DAO;
 
 import am.aua.resourcehub.util.ConnectionFactory;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AdminDAO {
-    public boolean validate(String username, String pass) {
-        String sql = "SELECT * FROM admin_user WHERE username = ? AND password = ?";
+    public boolean authenticate(String username, String password) {
+        String sql = "select * from admin_user where username = ? and password = ?";
         try(Connection conn = ConnectionFactory.getInstance().getConnection();
-            PreparedStatement statement = conn.prepareStatement(sql);
-        ){
-          statement.setString(1, username);
-          statement.setString(2, pass);
-          ResultSet rs = statement.executeQuery();
-          return rs.next();
-        } catch (SQLException e) {
+            PreparedStatement statement = conn.prepareStatement(sql)) {
+
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet rs = statement.executeQuery();
+
+            return rs.next();
+        } catch (SQLException e){
             e.printStackTrace();
         }
         return false;
     }
 }
-
---
+++ web/admin/editResource.jsp
@@ -0,0 +1,26 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: arevikagopoian
+  Date: 5/12/25
+  Time: 10:00 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page import="am.aua.resourcehub.DAO.ResourceDAO"%>
+<%@ page import="am.aua.resourcehub.DAO.ResourceDAOImpl"%>
+<%@ page import="am.aua.resourcehub.model.Resource" %>
+<%
+    int id = Integer.parseInt(request.getParameter("id"));
+    Resource resource = new ResourceDAO().getResourceById(id);
+%>
+<html>
+<head><title>Edit Resource</title></head>
+<body>
+<h2>Edit Resource</h2>
+<form action="editResource" method="post">
+  <input type="hidden" name="id" value="<%= resource.getId() %>"/>
+  Title: <input type="text" name="title" value="<%= resource.getTitle() %>" required/><br/>
+  Description: <input type="text" name="description" value="<%= resource.getDescription() %>" required/><br/>
+  <input type="submit" value="Update">
+  </form>
+</body>
+</html>
